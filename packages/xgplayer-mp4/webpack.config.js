d8ad6512f483ca68503258aacf4641ab9f478ee6
liyang007700 <liyang007700@163.com>
2018-07-13 19:34:55 +0800

    ADD: 增加mp4调试devserver

--- packages/xgplayer-mp4/webpack.config.js
+++ packages/xgplayer-mp4/webpack.config.js
@@ -1,75 +1,91 @@
-const polyfill = []
+// const polyfill = []
 
-const umd = {
-  entry: polyfill.concat(['./src/index.js']),
+// const umd = {
+//   entry: polyfill.concat(['./src/index.js']),
+//   output: {
+//     path: `${__dirname}/dist`,
+//     filename: 'index.js',
+//     library: 'xgplayer-mp4',
+//     libraryTarget: 'umd'
+//   },
+//   mode: 'production',
+//   module: {
+//     rules: [{
+//       test: /\.js$/,
+//       loader: 'babel-loader'
+//     }, {
+//       test: /\.scss$/,
+//       use: [
+//         'style-loader',
+//         {
+//           loader: 'css-loader',
+//           options: {
+//             importLoaders: 1,
+//             minimize: true
+//           }
+//         },
+//         'postcss-loader',
+//         'sass-loader'
+//       ]
+//     }]
+//   },
+//   externals: {
+//     'xgplayer': 'xgplayer'
+//   },
+//   optimization: {
+//     minimize: true
+//   }
+// }
+
+// const client = {
+//   entry: polyfill.concat(['./src/index.js']),
+//   output: {
+//     path: `${__dirname}/browser`,
+//     filename: 'index.js'
+//   },
+//   module: {
+//     rules: [{
+//       test: /\.js$/,
+//       loader: 'babel-loader'
+//     }]
+//   },
+//   externals: {
+//     'xgplayer': 'Player'
+//   },
+//   mode: 'production',
+//   optimization: {
+//     minimize: true
+//   }
+// }
+
+// module.exports = [umd, client]
+const path = require('path')
+const webpack = require('webpack')
+module.exports = {
+  entry: './src/index.js',
   output: {
-    path: `${__dirname}/dist`,
     filename: 'index.js',
-    library: 'xgplayer-mp4',
-    libraryTarget: 'umd'
+    path: path.resolve(__dirname, 'dist')
   },
-  mode: 'production',
-  module: {
-    rules: [{
-      test: /\.js$/,
-      loader: 'babel-loader'
-    }, {
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            importLoaders: 1,
-            minimize: true
-          }
-        },
-        'postcss-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  externals: {
-    'xgplayer': 'xgplayer'
-  },
-  optimization: {
-    minimize: true
-  }
-}
-
-const client = {
-  entry: polyfill.concat(['./src/index.js']),
-  output: {
-    path: `${__dirname}/browser`,
-    filename: 'index.js'
+  mode: 'development',
+  devServer: {
+    contentBase: './dist',
+    hot: true
   },
   module: {
     rules: [{
       test: /\.js$/,
       loader: 'babel-loader'
-    }, {
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            importLoaders: 1,
-            minimize: true
-          }
-        },
-        'postcss-loader',
-        'sass-loader'
-      ]
     }]
   },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   externals: {
-    'xgplayer': 'Player'
+    xgplayer: 'Player'
   },
-  mode: 'production',
   optimization: {
     minimize: true
   }
 }
-
-module.exports = [umd, client]
